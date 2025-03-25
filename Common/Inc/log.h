e038bb1c839258826c4900a14a5d8e2497c347b0
Caleb646 <calebthomas646@yahoo.com>
2025-03-25 16:38:34 -0400

    added logger macros

--- Common/Inc/log.h
+++ Common/Inc/log.h
@@ -1,11 +1,34 @@
 #ifndef LOG_H
 #define LOG_H
 
+#include <stdio.h>
 #include <stdint.h>
 #include "stm32h7xx_hal_uart.h"
 
-#define CM4_BUFF_ID 0
-#define CM7_BUFF_ID 1
+#define LOG_INFO(...)          \
+    do {                       \
+        printf ("[INFO] -- "); \
+        printf (__VA_ARGS__);  \
+        printf ("\r\n");       \
+    } while (0)
+#define LOG_DEBUG(...)          \
+    do {                        \
+        printf ("[DEBUG] -- "); \
+        printf (__VA_ARGS__);   \
+        printf ("\r\n");        \
+    } while (0)
+#define LOG_WARN(...)          \
+    do {                       \
+        printf ("[WARN] -- "); \
+        printf (__VA_ARGS__);  \
+        printf ("\r\n");       \
+    } while (0)
+#define LOG_ERROR(...)          \
+    do {                        \
+        printf ("[ERROR] -- "); \
+        printf (__VA_ARGS__);   \
+        printf ("\r\n");        \
+    } while (0)
 
 int8_t LoggerInit(UART_HandleTypeDef *pUART);
 
