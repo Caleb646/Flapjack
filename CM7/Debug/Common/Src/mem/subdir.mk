c7e69f11a864355a2d1b4653ef17f6aa0a89a61f
Caleb646 <calebthomas646@yahoo.com>
2025-06-20 16:42:37 -0400

    started on makefile

--- CM7/Debug/Common/Src/mem/subdir.mk
+++ CM7/Debug/Common/Src/mem/subdir.mk
@@ -1,27 +1,37 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-# Toolchain: GNU Tools for STM32 (13.3.rel1)
-################################################################################
+ifndef WORKDIR
+$(error WORKDIR is not defined. Use 'make WORKDIR=/path/to/root')
+endif
 
-# Add inputs and outputs from these tool invocations to the build variables 
-C_SRCS += \
-C:/Users/caleb/Projects/Drone/Common/Src/mem/ring_buff.c 
+SRC := $(WORKDIR)/Common/Src/mem/ring_buff.c
+OBJ := ./Common/Src/mem/ring_buff.o
+DEP := ./Common/Src/mem/ring_buff.d
 
-OBJS += \
-./Common/Src/mem/ring_buff.o 
+CC := arm-none-eabi-gcc
+CFLAGS := \
+  -mcpu=cortex-m7 -std=gnu11 -g3 -DDEBUG -DCORE_CM7 -DUSE_HAL_DRIVER -DSTM32H747xx -DUSE_PWR_DIRECT_SMPS_SUPPLY \
+  -c \
+  -I$(WORKDIR)/Core/Inc \
+  -I$(WORKDIR)/Drivers/STM32H7xx_HAL_Driver/Inc \
+  -I$(WORKDIR)/Drivers/STM32H7xx_HAL_Driver/Inc/Legacy \
+  -I$(WORKDIR)/Drivers/CMSIS/Device/ST/STM32H7xx/Include \
+  -I$(WORKDIR)/Drivers/CMSIS/Include \
+  -I$(WORKDIR)/Common/Inc \
+  -I$(WORKDIR)/Middlewares/Third_Party/FreeRTOS/Source/include \
+  -I$(WORKDIR)/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2 \
+  -I$(WORKDIR)/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F \
+  -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity \
+  -MMD -MP \
+  -MF"$(@:%.o=%.d)" -MT"$@" \
+  --specs=nano.specs -mfpu=fpv5-d16 -mfloat-abi=hard -mthumb
 
-C_DEPS += \
-./Common/Src/mem/ring_buff.d 
+# Build rule
+$(OBJ): $(SRC) Common/Src/mem/subdir.mk
+	$(CC) $(CFLAGS) -o "$@" "$<"
 
+# Clean rule
+clean: clean-mem
 
-# Each subdirectory must supply rules for building sources it contributes
-Common/Src/mem/ring_buff.o: C:/Users/caleb/Projects/Drone/Common/Src/mem/ring_buff.c Common/Src/mem/subdir.mk
-	arm-none-eabi-gcc "$<" -mcpu=cortex-m7 -std=gnu11 -g3 -DDEBUG -DCORE_CM7 -DUSE_HAL_DRIVER -DSTM32H747xx -DUSE_PWR_DIRECT_SMPS_SUPPLY -c -I../Core/Inc -I../../Drivers/STM32H7xx_HAL_Driver/Inc -I../../Drivers/STM32H7xx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32H7xx/Include -I../../Drivers/CMSIS/Include -I"C:/Users/caleb/Projects/Drone/Common/Inc" -I../../Middlewares/Third_Party/FreeRTOS/Source/include -I../../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2 -I../../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv5-d16 -mfloat-abi=hard -mthumb -o "$@"
-
-clean: clean-Common-2f-Src-2f-mem
-
-clean-Common-2f-Src-2f-mem:
+clean-mem:
 	-$(RM) ./Common/Src/mem/ring_buff.cyclo ./Common/Src/mem/ring_buff.d ./Common/Src/mem/ring_buff.o ./Common/Src/mem/ring_buff.su
 
-.PHONY: clean-Common-2f-Src-2f-mem
-
+.PHONY: clean clean-mem
--- Makefile
+++ Makefile
@@ -1,3 +1,87 @@
+WORKDIR := /Users/caleb/Projects/Drone
+
+# === Config ===
+TARGET      := firmware
+BUILD_DIR   := build
+CORES       := CM4 CM7
+
+# Shared source/include dirs
+COMMON_SRC  := $(WORKDIR)/Common/Src
+COMMON_INC  := $(WORKDIR)/Common/Inc
+
+# Compiler toolchain
+CC          := arm-none-eabi-gcc
+LD          := arm-none-eabi-gcc
+OBJCOPY     := arm-none-eabi-objcopy
+CFLAGS_BASE := -Wall -Werror -g -O0 -ffunction-sections -fdata-sections -std=gnu11
+LDFLAGS_BASE := -specs=nosys.specs -Wl,--gc-sections
+
+# Core-specific config
+CM4_MCU     := cortex-m4
+CM4_FPU     := fpv4-sp-d16
+CM4_FLOAT   := hard
+CM4_SRC     := cm4/src
+CM4_INC     := cm4/include
+CM4_LD      := cm4/linker.ld
+
+CM7_MCU     := cortex-m7
+CM7_FPU     := fpv5-d16
+CM7_FLOAT   := hard
+CM7_SRC     := \
+	$(WORKDIR)/CM7/Core/Inc \
+	$(WORKDIR)/CM7/Core/Startup \
+	$(WORKDIR)/CM7/Drivers/STM32H7xx_HAL_Driver \
+	$(WORKDIR)/CM7/Middlewares/Third_Party/FreeRTOS \
+CM7_INC     := cm7/include
+CM7_LD      := cm7/linker.ld
+
+# === Core-Specific build rules (generated per core) ===
+define BUILD_CORE
+
+SRC_$(1) := $$(shell find $$($(1)_SRC) -name '*.c')
+OBJ_$(1) := $$(patsubst %.c,$(BUILD_DIR)/$(1)/%.o,$$(SRC_$(1)))
+DEP_$(1) := $$(OBJ_$(1):%.o=%.d)
+INC_$(1) := -I$$($(1)_INC)
+
+CFLAGS_$(1) := $$(CFLAGS_BASE) -mcpu=$$($(1)_MCU) -mfpu=$$($(1)_FPU) -mfloat-abi=$$($(1)_FLOAT) -mthumb
+LDFLAGS_$(1) := $$(LDFLAGS_BASE) -T$$($(1)_LD) -mcpu=$$($(1)_MCU) -mfpu=$$($(1)_FPU) -mfloat-abi=$$($(1)_FLOAT) -mthumb
+
+ALL_ELFS += $(BUILD_DIR)/$(1)/$(TARGET).elf
+
+$(BUILD_DIR)/$(1)/$(TARGET).elf: $$($(OBJ_$(1)))
+	@mkdir -p $$(@D)
+	$$(LD) $$^ -o $$@ $$(LDFLAGS_$(1))
+
+$$(BUILD_DIR)/$(1)/%.o: %.c
+	@mkdir -p $$(@D)
+	$$(CC) $$(CFLAGS_$(1)) $$(INC_$(1)) -c $$< -o $$@
+
+-include $$($(DEP_$(1)))
+
+endef
+
+$(foreach core,$(CORES),$(eval $(call BUILD_CORE,$(core))))
+
+# === Targets ===
+
+all: $(ALL_ELFS)
+
+bin:
+	$(foreach core,$(CORES),\
+		$(OBJCOPY) -O binary $(BUILD_DIR)/$(core)/$(TARGET).elf $(BUILD_DIR)/$(core)/$(TARGET).bin; \
+	)
+
+flash: bin
+	st-flash write $(BUILD_DIR)/CM7/$(TARGET).bin 0x08000000
+	st-flash write $(BUILD_DIR)/CM4/$(TARGET).bin 0x08100000
+
+clean:
+	rm -rf $(BUILD_DIR)
+
+.PHONY: all bin flash clean
+
+
+
 all:
-	$(MAKE) -C ./CM4/Debug
-	$(MAKE) -C ./CM7/Debug
+	$(MAKE) -C ./CM4/Debug WORKDIR=$(WORKDIR)
+	$(MAKE) -C ./CM7/Debug WORKDIR=$(WORKDIR)
