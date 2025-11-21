cebfc431c46e2d84f4f181283037d57d76aba156
caleb646 <calebthomas646@gmail.com>
2025-11-20 20:38:16 -0500

    cmock

--- Tests/test.bash
+++ Tests/test.bash
@@ -4,4 +4,4 @@ cmake -S . -B build
 cmake --build build # --verbose 2>&1 | tee ./build/build_log.txt
 # ./build/Debug/test_queue.exe
 # ctest --test-dir build
-ctest --test-dir build -C Debug --output-on-failure
+# ctest --test-dir build -C Debug --output-on-failure
